fd5ab407b83f1f1bc57d6617841f9c675477e60c
Jarkko <jarkkohei@luukku.com>
2019-07-03 22:59:06 +0300

    Refactor MyApp from being a StatelessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer 1 chosen.');
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
@@ -21,7 +32,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(questions[0]),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
