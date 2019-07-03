beb7f3460e37b1c247568e58854cf201e4889503
Jarkko <jarkkohei@luukku.com>
2019-07-04 01:16:02 +0300

    Make questions -list contant

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favorite color?',
         'answers': ['Blue', 'Red', 'Green', 'Yellow']
