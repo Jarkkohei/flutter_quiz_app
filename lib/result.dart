4a151eb07770523ca0799861fc7d5c9d29fd1671
Jarkko <jarkkohei@luukku.com>
2019-07-04 02:52:16 +0300

    Implement keeping score and showing total score after the quiz

---
+++ lib/result.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You are strange!';
+    } else {
+      resultText = 'You are weirdo!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
